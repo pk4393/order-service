15e869a16464e29b7877134e86e66c4c315bfe5f
prashantkumar <prashant.kumar@gdn-commerce.com>
2025-02-19 15:00:22 +0530

    Improvements in the order report logic.

---
+++ src/main/java/com/example/order_service/response/ReportResponse.java
@@ -0,0 +1,21 @@
+package com.example.order_service.response;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReportResponse implements Serializable {
+  @Serial
+  private static final long serialVersionUID = -1592691973019497814L;
+  private long totalOrders;
+  private long totalProducts;
+  private Double totalRevenue;
+}
