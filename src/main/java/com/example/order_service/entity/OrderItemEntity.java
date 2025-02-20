5590f01fabecca244400233f0c6717cc6cfe2761
prashantkumar <prashant.kumar@gdn-commerce.com>
2025-02-20 22:49:06 +0530

    Validation improvements for the orders/report api call

--- src/main/java/com/example/order_service/entity/OrderItemEntity.java
+++ src/main/java/com/example/order_service/entity/OrderItemEntity.java
@@ -11,11 +11,16 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "order_items")
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
