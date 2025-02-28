b9277caf9d808fdace0f4bfda1c15f02da7e9f02
prashantkumar <prashant.kumar@gdn-commerce.com>
2025-02-28 11:29:55 +0530

    Improved the order report data and added the support to update the product stock via external api call

--- src/main/java/com/example/order_service/outbound/ProductApiClient.java
+++ src/main/java/com/example/order_service/outbound/ProductApiClient.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
 import org.springframework.web.service.annotation.PostExchange;
+import org.springframework.web.service.annotation.PutExchange;
 
 import com.example.order_service.outbound.model.Product;
 import com.example.order_service.request.UpdateProductRequest;
@@ -23,7 +23,7 @@ public interface ProductApiClient {
   @PostExchange("/find-by-ids")
   BaseResponse<List<Product>> findProductsByIds(@RequestBody List<Long> productIds);
 
-  @PutMapping("/{id}")
+  @PutExchange("/{id}")
   ResponseEntity<BaseResponse<Product>> updateProduct(@PathVariable long id,
       @RequestBody UpdateProductRequest request);
 }
