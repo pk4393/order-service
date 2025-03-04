63af4acc28bd60e5ba69bcbd7428110fda379757
prashantkumar <prashant.kumar@gdn-commerce.com>
2025-03-04 14:40:28 +0530

    Improved the order report data and added the support for the totalRevenue in the order report

--- src/main/java/com/example/order_service/response/ProductsResponse.java
+++ src/main/java/com/example/order_service/response/ProductsResponse.java
@@ -1,13 +1,14 @@
 package com.example.order_service.response;
 
+import java.io.Serializable;
+
 import com.example.order_service.outbound.model.Product;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.io.Serializable;
 
 @Getter
 @NoArgsConstructor
@@ -22,12 +23,13 @@ public class ProductsResponse implements Serializable {
     private Double price;
     private String category;
     private String brand;
-    private Product product;
 
     public ProductsResponse(Product product) {
         this.id = product.getId();
         this.title = product.getTitle();
         this.price = product.getPrice();
         this.description = product.getDescription();
+        this.brand = product.getBrand();
+        this.category = product.getCategory();
     }
 }
